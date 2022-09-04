726192417503098528225b8955993233de47e03e
Kanishk Harde <66664028+kanishkharde@users.noreply.github.com>
2022-09-04 15:20:48 -0400

    Create celsius.py

---
+++ code/celsius.py
@@ -0,0 +1,10 @@
+import sys
+def convert(cel):
+    # calculate fahrenheit
+    fahrenheit = (cel * 1.8) + 32
+    print('%0.1f degree Celsius = %0.1f degree Fahrenheit' % (cel, fahrenheit))
+
+if __name__ == '__main__':
+    arg = sys.argv
+    n = float(arg[1])
+    convert(n)
