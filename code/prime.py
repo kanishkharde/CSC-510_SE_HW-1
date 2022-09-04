7e61c86bff2401c9c6fb9efd279713471253beaa
Kanishk Harde <66664028+kanishkharde@users.noreply.github.com>
2022-09-04 15:15:12 -0400

    Create prime.py

---
+++ code/prime.py
@@ -0,0 +1,20 @@
+import sys
+def prime(num):
+    if num == 1:
+        print("1 is neither prime nor composite")
+    elif num > 1:
+        # check for factors
+        for i in range(2, num):
+            if (num % i) == 0:
+                print(num, "is not a prime number")
+                print(i, "times", num // i, "is", num)
+                break
+        else:
+            print(num, "is a prime number")
+    else:
+        print(num, "is not a postive nonzero number")
+
+if __name__ == '__main__':
+    arg = sys.argv
+    n = int(arg[1])
+    print(prime(n))
