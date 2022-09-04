a7817aa7941a2055429db0822a94025ede42294c
Kanishk Harde <66664028+kanishkharde@users.noreply.github.com>
2022-09-04 15:24:09 -0400

    Create test.py

---
+++ test/test.py
@@ -0,0 +1,18 @@
+import unittest
+
+from prime import prime_check
+from celsius import convert
+
+class TestReturnValues(unittest.TestCase):
+    
+    def test_prime(self):
+        """
+        Test for a known return value
+        """
+        self.assertEqual(prime_check(5), 120)
+
+    def test_convert(self):
+        """
+        Test for a known return value
+        """
+        self.assertEqual(convert(9), 34.5)
