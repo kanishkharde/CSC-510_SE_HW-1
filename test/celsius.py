2cfe429d0728840a5124cae6621ffec72ef21869
Kanishk Harde <66664028+kanishkharde@users.noreply.github.com>
2022-09-05 00:26:39 -0400

    Update celsius.py

--- test/celsius.py
+++ test/celsius.py
@@ -3,6 +3,7 @@ def convert(cel):
     # calculate fahrenheit
     fahrenheit = (cel * 1.8) + 32
     print('%0.1f degree Celsius = %0.1f degree Fahrenheit' % (cel, fahrenheit))
+    return fahrenheit
 
 if __name__ == '__main__':
     arg = sys.argv
